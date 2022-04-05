686a6c9b98b834b4641fd6eee391ecb93e89b57d
Viktorija Vodilovska <viktorija.vodilovska@gmail.com>
2022-04-05 16:55:06 +0200

    Initial commit

---
+++ src/main/java/com/group18/getapet/GetapetApplication.java
@@ -0,0 +1,13 @@
+package com.group18.getapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetapetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetapetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group18/getapet/GetapetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group18.getapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetapetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
